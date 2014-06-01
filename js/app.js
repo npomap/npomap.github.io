063bce158bd9d579bfac6bd3bc4b14071c57d374
Phil Schumm <pschumm@uchicago.edu>
2014-05-31 19:59:39 -0500

    Started basic template

---
+++ js/app.js
@@ -0,0 +1,7 @@
+var NPOMapApp = angular.module('NPOMapApp',[]);
+
+NPOMapApp.controller('NPOMapAppCtrl', function($scope) {
+    
+    $scope.categories = ["Health","Education","Food","Safety","Civil Rights"];
+    
+});

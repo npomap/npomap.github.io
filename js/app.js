fcd4de1796c0c9b795bbeaacfe6072d1569c3e74
Phil Schumm <pschumm@uchicago.edu>
2014-06-01 02:46:23 -0500

    Minimal functionality now complete

--- js/app.js
+++ js/app.js
@@ -2,21 +2,63 @@ var NPOMapApp = angular.module('NPOMapApp',[]);
 
 NPOMapApp.controller('NPOMapAppCtrl', function($scope) {
     
-    $scope.categories = ["Civil Rights","Education","Food","Health","Safety"];
+    $scope.categories = [['Civil Rights','civil_rights'],
+                         ['Education','education'],
+                         ['Food','food'],
+                         ['Health','health'],
+                         ['Safety','safety']];
     
     $scope.codeAddress = function() {
       var address = document.getElementById("address").value;
       geocoder.geocode( { 'address': address}, function(results, status) {
         if (status == google.maps.GeocoderStatus.OK) {
           map.setCenter(results[0].geometry.location);
-          var marker = new google.maps.Marker({
-              map: map,
-              position: results[0].geometry.location
-          });
         } else {
           alert("Geocode was not successful for the following reason: " + status);
         }
       });
+    }
+    
+    function clearMarkers() {
+        if ('markersArray' in window) {
+            for (i in markersArray) {
+                markersArray[i].setMap(null);
+            }
+        }
+    }
+    
+    $scope.placePins = function() {
+        
+        if (data[$scope.formData.category] != null) {
+            
+            clearMarkers();
+            var mapArray = data[$scope.formData.category];
+            markersArray = [];
+            var infowindow = new google.maps.InfoWindow();
+            
+            for (var i in mapArray) {
+                var thislatlng = new google.maps.LatLng(mapArray[i]['Latitude'], mapArray[i]['Longitude']),
+                contentString = mapArray[i]['Name'] + '<br />' + mapArray[i]['Address'],
+                marker = new google.maps.Marker({
+                    map: map,
+                    position: thislatlng,
+                    information: contentString
+                });
+                
+                google.maps.event.addListener(marker, 'click', function() {
+                    infowindow.setContent(this.information);
+                    infowindow.open(map, this);
+                });
+                
+                markersArray.push(marker);
+            }
+        } else if ($scope.formData.category == '') {
+            clearMarkers();
+        } else {
+            alert('Not available yet!');
+            $('#category').select2('val', '');
+            clearMarkers();
+        }
     }
     
 });
---
+++ js/data.js
@@ -0,0 +1,86 @@
+data = {'health': [
+  {
+    "ID":1,
+    "Name":"Englewood Neighborhood Health Clinic",
+    "Address":"641 W. 63rd St.",
+    "City":"Chicago",
+    "State":"IL",
+    "ZIP":60621,
+    "GeographicArea":"",
+    "Latitude":"41.77943",
+    "Longitude":"-87.64146",
+    "Location":""
+  },
+  {
+    "ID":2,
+    "Name":"Lower West Neighborhood Health Clinic",
+    "Address":"1713 S. Ashland Ave.",
+    "City":"Chicago",
+    "State":"IL",
+    "ZIP":60608,
+    "GeographicArea":"",
+    "Latitude":"41.85820",
+    "Longitude":"-87.66584",
+    "Location":""
+  },
+  {
+    "ID":3,
+    "Name":"Roseland Neighborhood Health Clinic",
+    "Address":"200 E. 115th St.",
+    "City":"Chicago",
+    "State":"IL",
+    "ZIP":60628,
+    "GeographicArea":"",
+    "Latitude":"41.68548",
+    "Longitude":"-87.61749",
+    "Location":""
+  },
+  {
+    "ID":4,
+    "Name":"South Chicago Women & Children Health Center",
+    "Address":"2938 E. 89th St.",
+    "City":"Chicago",
+    "State":"IL",
+    "ZIP":60617,
+    "GeographicArea":"",
+    "Latitude":"41.73383",
+    "Longitude":"-87.55259",
+    "Location":""
+  },
+  {
+    "ID":5,
+    "Name":"South Lawndale Women & Children Health Center",
+    "Address":"3059 W. 26th St.",
+    "City":"Chicago",
+    "State":"IL",
+    "ZIP":60623,
+    "GeographicArea":"",
+    "Latitude":"41.84408",
+    "Longitude":"-87.70191",
+    "Location":""
+  },
+  {
+    "ID":6,
+    "Name":"Uptown Neighborhood Health Center",
+    "Address":"845 W. Wilson Ave.",
+    "City":"Chicago",
+    "State":"IL",
+    "ZIP":60640,
+    "GeographicArea":"",
+    "Latitude":"41.96529",
+    "Longitude":"-87.65199",
+    "Location":""
+  },
+  {
+    "ID":7,
+    "Name":"West Town Neighborhood Health Center",
+    "Address":"2418 W. Division St.",
+    "City":"Chicago",
+    "State":"IL",
+    "ZIP":60622,
+    "GeographicArea":"",
+    "Latitude":"41.90324",
+    "Longitude":"-87.68781",
+    "Location":""
+  }
+]};

645e9b61d846f6fa06ee5ebec24b9482b6f59cc1
Phil Schumm <pschumm@uchicago.edu>
2014-05-31 20:28:04 -0500

    Begin incorporating Sascha's code

--- js/app.js
+++ js/app.js
@@ -4,4 +4,19 @@ NPOMapApp.controller('NPOMapAppCtrl', function($scope) {
     
     $scope.categories = ["Health","Education","Food","Safety","Civil Rights"];
     
+    $scope.codeAddress = function() {
+      var address = document.getElementById("address").value;
+      geocoder.geocode( { 'address': address}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          map.setCenter(results[0].geometry.location);
+          var marker = new google.maps.Marker({
+              map: map,
+              position: results[0].geometry.location
+          });
+        } else {
+          alert("Geocode was not successful for the following reason: " + status);
+        }
+      });
+    }
+    
 });
